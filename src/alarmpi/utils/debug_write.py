3a2bafaf22d9cf640f9d2807c63c3c2b8fb0c4b4
Lauri Ajanki <lauri.ajanki@gmail.com>
2025-03-22 17:54:53 +0200

    chore: adjust debug output formatting

--- src/alarmpi/utils/debug_write.py
+++ src/alarmpi/utils/debug_write.py
@@ -20,23 +20,31 @@ class DateTimeEncoder(json.JSONEncoder):
 
 
 def signal_handler(config, sig=None, frame=None):
-    """Dump current state to file."""
+    """Dump current program state to file."""
     OUTPUT_FILE = "debug_info.txt"
     app = QApplication.instance()
 
     with open(OUTPUT_FILE, "w") as f:
         f.write(f"Config snapshot from: {datetime.now().isoformat()}\n")
+        
+        # Highlight config file name
+        f.write("\n=== Config File Name ===\n")
         f.write(f"config file: {config.config_file}\n")
+        
+        # Highlight config file content
+        f.write("\n=== Config File Content ===\n")
         json.dump(config.config, f, indent=4, cls=DateTimeEncoder)
-
+        
+        # Highlight window states
+        f.write("\n=== Window States ===\n")
         f.write(
-            "\n{:15} {:9} {:12} {:14} {:11}".format(
+            "\n{:<20} {:<10} {:<15} {:<15} {:<10}".format(
                 "window", "isVisible", "isFullScreen", "isActiveWindow", "isEnabled"
             )
         )
         for window in app.topLevelWidgets():
             f.write(
-                "\n{:15} {:9} {:12} {:14} {:11}".format(
+                "\n{:<20} {:<10} {:<15} {:<15} {:<10}".format(
                     window.__class__.__name__,
                     window.isVisible(),
                     window.isFullScreen(),
