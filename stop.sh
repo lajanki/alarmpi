77cf2aff5962e6b02a87137c8ae7eee74474cd0c
Lauri Ajanki <lauri.ajanki@gmail.com>
2025-08-23 18:00:31 +0300

    feat: check process before sending signals

--- stop.sh
+++ stop.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# Get the process ID of the running alarmpi process
-pid=$(pgrep -f "bin/alarmpi")
 
-# Send a debug signal
-kill -s USR1 $pid
-
-
-# stop any running alarms
-pkill cvlc
-pkill -f "play_alarm.py"
-kill $pid
-
-# Ensure backlight is turned on (only on Raspberry Pi)
+# Ensure backlight is turned on (Raspberry Pi)
 if [[ -d "/sys/class/backlight/rpi_backlight" ]]; then
     echo 0 > /sys/class/backlight/rpi_backlight/bl_power
     echo 255 > /sys/class/backlight/rpi_backlight/brightness
@@ -22,3 +11,18 @@ if [[ -d "/sys/class/backlight/10-0045" ]]; then
     echo 0 > /sys/class/backlight/10-0045/bl_power
     echo 255 > /sys/class/backlight/10-0045/brightness
 fi
+
+# Find the main alarmpi process and stop it if found
+pid=$(pgrep -f "bin/alarmpi")
+
+if [ -z "$pid" ]; then
+    exit 0
+fi
+
+# Send a debug signal
+kill -s USR1 $pid
+
+# Stop any running alarms
+pkill cvlc
+pkill -f "play_alarm.py"
+kill $pid
